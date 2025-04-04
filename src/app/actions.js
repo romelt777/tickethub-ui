7a5eecc4e0e5f3c7236df5c926172543fdf399d5
romelt777 <romel.tan777@gmail.com>
2025-04-03 23:44:10 -0300

    able to submit form locally

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,7 +3,7 @@
 
 //sending form to api
 export const formSubmit = async (formData) => {
-    const response = await fetch('YOUR_API_ENDPOINT', {
+    const response = await fetch('https://04165-api-tickethub-fnh5frg4g3abcuhh.canadacentral-01.azurewebsites.net/api/ticketorder', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -12,8 +12,8 @@ export const formSubmit = async (formData) => {
     });
 
     if (response.ok) {
-        alert('Order submitted successfully!');
+        return ('Order submitted successfully!');
     } else {
-        alert('Error submitting order.');
+        return ('Error submitting order.');
     }
 }
